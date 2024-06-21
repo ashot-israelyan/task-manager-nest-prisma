9d3d9e6fe864eea46cec6df0b8e7a22a34162b99
Ashot Israelyan <ashot.israelyan@tumo.org>
2024-06-21 16:52:10 +0400

    feat: init statistics page

--- red-planner-frontend/src/app/i/page.tsx
+++ red-planner-frontend/src/app/i/page.tsx
@@ -1,14 +1,21 @@
 import type { Metadata } from 'next';
 
+import { Heading } from '@/components/ui/Heading';
+
 import { NO_INDEX_PAGE } from '@/constants/seo.constants';
+
+import Staistics from './Staistics';
 
 export const metadata: Metadata = {
 	title: 'Dashboard',
 	...NO_INDEX_PAGE,
 };
 
-const DashboardPage = () => {
-	return <div>Dashboard</div>;
-};
-
-export default DashboardPage;
+export default function DashboardPage() {
+	return (
+		<div>
+			<Heading title='Statistics' />
+			<Staistics />
+		</div>
+	);
+}
