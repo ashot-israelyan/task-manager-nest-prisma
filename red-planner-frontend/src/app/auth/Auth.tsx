2073453a03df3ae7fd332c3d6f9a9e69b175aef8
Ashot Israelyan <ashot.israelyan@tumo.org>
2024-06-21 16:18:32 +0400

    feat: implement middleware for auth

--- red-planner-frontend/src/app/auth/Auth.tsx
+++ red-planner-frontend/src/app/auth/Auth.tsx
@@ -6,6 +6,10 @@ import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 
+import { Heading } from '@/components/ui/Heading';
+import { Button } from '@/components/ui/buttons/Button';
+import { Field } from '@/components/ui/fields/Field';
+
 import { IAuthForm } from '@/types/auth.types';
 
 import { DASHBOARD_PAGES } from '@/config/pages-url.config';
@@ -41,9 +45,31 @@ const Auth = () => {
 				className='w-1/4 m-auto shadow bg-sidebar rounded-xl p-layout'
 				onSubmit={handleSubmit(onSubmit)}
 			>
-				{/* HEADING */}
-				{/* FIELDS */}
-				<div className='flex items-center gap-5 justify-center'>{/* BUTTONS */}</div>
+				<Heading title='Auth' />
+				<Field
+					id='email'
+					label='Email:'
+					placeholder='Enter email:'
+					type='email'
+					extra='mb-4'
+					{...register('email', {
+						required: 'Email is required',
+					})}
+				/>
+				<Field
+					id='password'
+					label='Password:'
+					placeholder='Enter password:'
+					type='password'
+					extra='mb-4'
+					{...register('password', {
+						required: 'Password is required',
+					})}
+				/>
+				<div className='flex items-center gap-5 justify-center'>
+					<Button onClick={() => setIsLoginForm(true)}>Login</Button>
+					<Button onClick={() => setIsLoginForm(false)}>Register</Button>
+				</div>
 			</form>
 		</div>
 	);
